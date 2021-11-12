c36cc07578ae4daea70b9796990d4468b0cd7bbc
Liiner <liiner69@mail.ru>
2021-11-12 08:08:43 +0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        //погода на 5 дней Ижевск
+        String cityCode = RequestHandler.detectCityId("Izhevsk");
+        String forecastFiveDay = RequestHandler.getForecast(cityCode);
+        System.out.println(cityCode);
+        System.out.println(forecastFiveDay);
+
+        //погода на 1 день Воронеж
+        String cityCode1 = RequestHandler.detectCityId("Voronezh");
+        String forecastOneDay = RequestHandler.getForecastOneDay(cityCode1);
+        System.out.println(cityCode1);
+        System.out.println(forecastOneDay);
+    }
+}
